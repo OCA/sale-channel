cb35efd48350110f133df400589abf9da9a3d197
kevinkhao <kevin.khao@akretion.com.br>
2020-04-30 16:44:48 +0200

    sale_channel: add module

---
+++ sale_channel/__manifest__.py
@@ -0,0 +1,19 @@
+#  Copyright (c) Akretion 2020
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+{
+    "name": "Sale Channel",
+    "summary": "Adds the notion of sale channels",
+    "version": "12.0.1.0.0",
+    "category": "Generic Modules/Sale",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/sale-import",
+    "depends": ["sale_management"],
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order.xml",
+        "views/account_invoice.xml",
+        "views/sale_channel.xml",
+    ],
+    "demo": ["demo/demo.xml"],
+}
---
+++ sale_channel/models/sale_channel.py
@@ -0,0 +1,11 @@
+#  Copyright (c) Akretion 2020
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+from odoo import fields, models
+
+
+class SaleChannel(models.Model):
+    _name = "sale.channel"
+    _description = "Sale Channel"
+
+    name = fields.Char("Name", required=True)
