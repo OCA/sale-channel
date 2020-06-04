3674036624bfeee3d810667661860e259940c29b
KevinKhao <kevinkhao@gmail.com>
2020-06-04 22:29:26 +0200

    [IMP] Rename to sale_channel_partner, fix domain on smart button, use _prepare_invoices instead of _finalize_invoices, misc PR feedback

---
+++ sale_channel_partner/__manifest__.py
@@ -0,0 +1,19 @@
+#  Copyright (c) Akretion 2020
+#  License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+{
+    "name": "Sale Channel Partner",
+    "summary": "Bind sale channels to contacts",
+    "version": "12.0.1.0.0",
+    "category": "Generic Modules/Sale",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/sale-import",
+    "depends": ["sale_channel"],
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_channel.xml",
+        "views/res_partner.xml",
+        "views/sale_channel_partner.xml",
+    ],
+    "demo": ["demo/demo.xml"],
+}
