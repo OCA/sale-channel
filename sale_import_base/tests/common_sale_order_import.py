2af36a243d1bda1a0188d063953cb9e5a7d18143
KevinKhao <kevinkhao@gmail.com>
2020-06-05 17:30:27 +0200

    [IMP] Akretion copyrights

--- sale_import_base/tests/common_sale_order_import.py
+++ sale_import_base/tests/common_sale_order_import.py
@@ -1,5 +1,5 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-
+#  Copyright (c) Akretion 2020
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from copy import deepcopy
 
 from odoo.addons.component.tests.common import SavepointComponentCase
