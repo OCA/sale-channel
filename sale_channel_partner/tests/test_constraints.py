2136da3aecbec5a8023614d242675a0eb9d440fc
KevinKhao <kevinkhao@gmail.com>
2020-06-05 11:51:45 +0200

    [IMP] Solve issues with Travis tests and psycopg

--- sale_channel_partner/tests/test_constraints.py
+++ sale_channel_partner/tests/test_constraints.py
@@ -1,8 +1,12 @@
 #  Copyright (c) Akretion 2020
 #  License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+# pylint: disable=missing-manifest-dependency
+
 from psycopg2 import IntegrityError
 
 from odoo.tests import TransactionCase
+from odoo.tools import mute_logger
 
 
 class TestConstraints(TransactionCase):
@@ -14,9 +18,9 @@ class TestConstraints(TransactionCase):
 
     def test_constraint_channel_extid(self):
         new_partner = self.env.ref("base.res_partner_address_31")
-        with self.assertRaises(IntegrityError):
+        with self.assertRaises(IntegrityError), mute_logger("odoo.sql_db"):
             self.binding.copy({"partner_id": new_partner.id})
 
     def test_constraint_channel_partner(self):
-        with self.assertRaises(IntegrityError):
+        with self.assertRaises(IntegrityError), mute_logger("odoo.sql_db"):
             self.binding.copy({"external_id": "new external id"})
