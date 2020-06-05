489c383b456ed8820e573b1684530f08e27043b4
KevinKhao <kevinkhao@gmail.com>
2020-06-05 12:26:45 +0200

    [ADD] sale_import_base, sale_import_delivery_carrier, sale_import_rest

---
+++ sale_import_rest/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Import REST",
+    "summary": "REST API for importig Sale Orders",
+    "version": "12.0.1.0.0",
+    "category": "Generic Modules/Sale",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/sale-import",
+    "depends": ["sale_import_base", "auth_api_key"],
+    "license": "AGPL-3",
+    "data": ["views/sale_channel.xml"],
+    "demo": ["demo/demo.xml"],
+    "installable": True,
+}
---
+++ sale_import_rest/models/sale_channel.py
@@ -0,0 +1,11 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleChannel(models.Model):
+    _inherit = "sale.channel"
+
+    api_key = fields.Many2one(
+        "auth.api.key", string="REST Api Key", ondelete="set null"
+    )
