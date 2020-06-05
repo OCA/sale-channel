32003b0eff25512f9cd8748293dda3db4e39f4a9
KevinKhao <kevinkhao@gmail.com>
2020-06-05 18:16:56 +0200

    [IMP] remove _si prefixes on processor component

--- sale_import_base/components/importer_sale_channel.py
+++ sale_import_base/components/importer_sale_channel.py
@@ -39,40 +39,40 @@ class ImporterSaleChannel(Component):
             raise
         json_data_initial = so_datamodel_load.dump()
         json_data = deepcopy(json_data_initial)
-        so_vals = self._si_process_data(json_data)
+        so_vals = self.process_data(json_data)
         new_sale_order = self.env["sale.order"].create(so_vals)
-        self._si_finalize(new_sale_order, json_data_initial)
+        self.finalize(new_sale_order, json_data_initial)
         return new_sale_order
 
     # DATAMODEL PROCESSORS
-    def _si_process_data(self, so_vals):
+    def _process_data(self, so_vals):
         """ Transform values in-place
          to make it usable in create() """
-        self._si_process_simple_fields(so_vals)
-        self._si_process_m2os(so_vals)
-        so_vals = self._si_simulate_onchanges(so_vals)
+        self.process_simple_fields(so_vals)
+        self.process_m2os(so_vals)
+        so_vals = self.simulate_onchanges(so_vals)
         return so_vals
 
-    def _si_process_simple_fields(self, so_vals):
+    def _process_simple_fields(self, so_vals):
         del so_vals["currency_code"]
         del so_vals["payment"]
 
-    def _si_process_m2os(self, so_vals):
-        channel_id = self._si_get_channel(so_vals)
-        partner_id = self._si_get_partner(so_vals, channel_id)
-        self._si_process_addresses(partner_id, so_vals)
-        self._si_process_lines(so_vals)
-        self._si_process_amount(so_vals)
-        self._si_process_invoice(so_vals)
-        self._si_process_sale_channel(so_vals)
+    def _process_m2os(self, so_vals):
+        channel_id = self.get_channel(so_vals)
+        partner_id = self.get_partner(so_vals, channel_id)
+        self.process_addresses(partner_id, so_vals)
+        self.process_lines(so_vals)
+        self.process_amount(so_vals)
+        self.process_invoice(so_vals)
+        self.process_sale_channel(so_vals)
 
-    def _si_get_channel(self, so_vals):
+    def _get_channel(self, so_vals):
         sale_channel = self.env["sale.channel"].search(
             [("name", "=", so_vals["sale_channel"])]
         )
         return sale_channel
 
-    def _si_get_partner(self, so_vals, sale_channel):
+    def _get_partner(self, so_vals, sale_channel):
         external_id = so_vals["address_customer"].get("external_id")
         binding = self.env["sale.channel.partner"].search(
             [
@@ -88,9 +88,9 @@ class ImporterSaleChannel(Component):
             )
             if partner:
                 return partner
-        return self._si_create_partner(so_vals)
+        return self.create_partner(so_vals)
 
-    def _si_create_partner(self, so_vals):
+    def _create_partner(self, so_vals):
         partner_vals = dict()
         for field in MAPPINGS_SALE_ORDER_ADDRESS_SIMPLE:
             partner_vals[field] = so_vals["address_customer"][field]
@@ -101,7 +101,7 @@ class ImporterSaleChannel(Component):
         partner = self.env["res.partner"].create(partner_vals)
         return partner
 
-    def _si_process_addresses(self, partner_id, so_vals):
+    def _process_addresses(self, partner_id, so_vals):
         # customer itself
         vals_addr_customer = so_vals["address_customer"]
         new_state = (
@@ -156,7 +156,7 @@ class ImporterSaleChannel(Component):
             so_vals[field] = version.id
             del so_vals[vals_key]
 
-    def _si_process_lines(self, so_vals):
+    def _process_lines(self, so_vals):
         lines = so_vals["lines"]
         so_vals["order_line"] = list()
         for line in lines:
@@ -180,17 +180,17 @@ class ImporterSaleChannel(Component):
             so_vals["order_line"].append(line_vals_command)
         del so_vals["lines"]
 
-    def _si_process_amount(self, so_vals):
+    def _process_amount(self, so_vals):
         for k, v in so_vals["amount"].items():
             si_key = "si_" + k
             so_vals[si_key] = v
         del so_vals["amount"]
 
-    def _si_process_invoice(self, so_vals):
+    def _process_invoice(self, so_vals):
         # TODO actually use that val
         del so_vals["invoice"]
 
-    def _si_process_sale_channel(self, so_vals):
+    def _process_sale_channel(self, so_vals):
         channel = self.env["sale.channel"].search(
             [("name", "=", so_vals.get("sale_channel"))]
         )
@@ -198,7 +198,7 @@ class ImporterSaleChannel(Component):
             so_vals["sale_channel_id"] = channel.id
             del so_vals["sale_channel"]
 
-    def _si_simulate_onchanges(self, order):
+    def _simulate_onchanges(self, order):
         """ Drawn from connector_ecommerce module with modifications:
         onchange fields, some syntax
         Play the onchange of the sales order and it's lines
@@ -220,7 +220,7 @@ class ImporterSaleChannel(Component):
         line_onchange_fields = ["product_id"]
 
         # play onchange on sales order
-        order = self._si_simulate_onchanges_play_onchanges(
+        order = self.simulate_onchanges_play_onchanges(
             "sale.order", order, order_onchange_fields
         )
 
@@ -236,7 +236,7 @@ class ImporterSaleChannel(Component):
                     old_line_data = deepcopy(command_line[2])
                     # give a virtual order_id for the fiscal position/taxes
                     old_line_data["order_id"] = self.env["sale.order"].new(order)
-                    new_line_data = self._si_simulate_onchanges_play_onchanges(
+                    new_line_data = self.simulate_onchanges_play_onchanges(
                         "sale.order.line", old_line_data, line_onchange_fields
                     )
                     del new_line_data["order_id"]
@@ -246,7 +246,7 @@ class ImporterSaleChannel(Component):
                     line_list[idx] = new_line
         return order
 
-    def _si_simulate_onchanges_play_onchanges(self, model, values, onchange_fields):
+    def _simulate_onchanges_play_onchanges(self, model, values, onchange_fields):
         model = self.env[model]
         onchange_specs = model._onchange_spec()
 
@@ -263,7 +263,7 @@ class ImporterSaleChannel(Component):
         new_values = {}
         for field in onchange_fields:
             onchange_values = new_record.onchange(all_values, field, onchange_specs)
-            new_values_diff = self._si_simulate_onchanges_get_new_values(
+            new_values_diff = self.simulate_onchanges_get_new_values(
                 values, onchange_values, model=model._name
             )
             new_values.update(new_values_diff)
@@ -272,9 +272,7 @@ class ImporterSaleChannel(Component):
         res = {f: v for f, v in all_values.items() if f in values or f in new_values}
         return res
 
-    def _si_simulate_onchanges_get_new_values(
-        self, record, on_change_result, model=None
-    ):
+    def _simulate_onchanges_get_new_values(self, record, on_change_result, model=None):
         vals = on_change_result.get("value", {})
         new_values = {}
         for fieldname, value in vals.items():
@@ -287,12 +285,12 @@ class ImporterSaleChannel(Component):
         return new_values
 
     # FINALIZERS
-    def _si_finalize(self, new_sale_order, raw_import_data):
+    def _finalize(self, new_sale_order, raw_import_data):
         """ Extend to add final operations """
-        self._si_sync_binding(new_sale_order, raw_import_data)
-        self._si_create_payment(new_sale_order, raw_import_data)
+        self.sync_binding(new_sale_order, raw_import_data)
+        self.create_payment(new_sale_order, raw_import_data)
 
-    def _si_sync_binding(self, sale_order, data):
+    def _sync_binding(self, sale_order, data):
         if not data["address_customer"].get("external_id"):
             return
         existing_binding = self.env["sale.channel.partner"].search(
@@ -309,7 +307,7 @@ class ImporterSaleChannel(Component):
             }
             self.env["sale.channel.partner"].create(binding_vals)
 
-    def _si_create_payment(self, sale_order, data):
+    def _create_payment(self, sale_order, data):
         if not data.get("payment"):
             return
         pmt_data = data["payment"]
@@ -329,7 +327,7 @@ class ImporterSaleChannel(Component):
         new_pmt = self.env["payment.transaction"].create(payment_vals)
         sale_order.transaction_ids = new_pmt
 
-    def helper_find_binding(self, sale_channel, external_id):
+    def _helper_find_binding(self, sale_channel, external_id):
         binding = self.env["sale.channel.partner"].search(
             [
                 ("external_id", "=", external_id),
