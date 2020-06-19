dbbd478e9252c86e68c505bc2fbcd3b32b373585
KevinKhao <kevinkhao@gmail.com>
2020-06-19 22:22:55 +0200

    [FIX] tests and service logic

--- sale_import_rest/components/sale_import_service.py
+++ sale_import_rest/components/sale_import_service.py
@@ -1,5 +1,8 @@
 #  Copyright (c) Akretion 2020
 #  License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+from odoo import _
+from odoo.exceptions import ValidationError
+
 from odoo.addons.component.core import Component
 
 
@@ -7,42 +10,25 @@ class SaleImportService(Component):
     _inherit = "base.rest.service"
     _name = "sale.import.service"
     _usage = "import"
-    _collection_name = "sale.import.rest.services"
+    _collection_name = "sale.import.endpoints"
     _default_auth = "public"
 
     def create(self, api_key, sale_order_data, **params):
-        api_key_id = self.env["auth.api.key"].search([("api_key", "=", api_key)])
-        if not api_key_id:
-            return {"response": "Couldn't find a matching API key"}
+        api_key_id = self.env["auth.api.key"]._retrieve_api_key(api_key)
         sale_channel = self.env["sale.channel"].search(
             [("api_key", "=", api_key_id.id)]
         )
         if not sale_channel:
-            return {"response": "API key does not map to any sale channel"}
-        try:
-            vals = [
-                {
-                    "usage": "json_import",
-                    "apply_on_model": "sale.order",
-                    "data_str": data_str,
-                    "model_name": "sale.channel",
-                    "record_id": sale_channel.id,
-                }
-                for data_str in sale_order_data
-            ]
-            chunk_ids = self.env["queue.job.chunk"].create(vals)
-        except Exception as e:
-            return {"response": "Error while trying to create jobs: %s" % str(e)}
+            raise ValidationError(_("API key does not map to any sale channel"))
+        vals = [
+            {
+                "usage": "json_import",
+                "apply_on_model": "sale.order",
+                "data_str": data_str,
+                "model_name": "sale.channel",
+                "record_id": sale_channel.id,
+            }
+            for data_str in sale_order_data
+        ]
+        chunk_ids = self.env["queue.job.chunk"].create(vals)
         return {"response": chunk_ids.ids}
-
-    def get(self, _id, message):
-        return {"response": "Get is not supported"}
-
-    def search(self, message):
-        return {"response": "Search is not supported"}
-
-    def update(self, _id, message):
-        return {"response": "PUT is not supported"}
-
-    def delete(self, _id):
-        return {"response": "DELETE is not supported"}
