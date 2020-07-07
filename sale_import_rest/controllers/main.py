68f7ec45a19c203fc8842bbd96b2f601dbd48249
KevinKhao <kevinkhao@gmail.com>
2020-07-07 16:15:12 +0200

    Cleanup

--- sale_import_rest/controllers/main.py
+++ sale_import_rest/controllers/main.py
@@ -6,5 +6,4 @@ from odoo.addons.base_rest.controllers.main import RestController
 class SaleImportBaseController(RestController):
     _root_path = "/sale_import_rest/"
     _collection_name = "sale.import.rest.services"
-    # TODO change
     _default_auth = "api_key"
