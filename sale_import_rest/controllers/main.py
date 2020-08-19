f08bbc0e76eff950b1eac6cc6cea8aa1c9011a3f
Sébastien BEAU <sebastien.beau@akretion.com>
2020-08-19 19:34:10 +0200

    [FIX] fix issue with @property and doc, better name for root path

--- sale_import_rest/controllers/main.py
+++ sale_import_rest/controllers/main.py
@@ -4,6 +4,6 @@ from odoo.addons.base_rest.controllers.main import RestController
 
 
 class SaleImportBaseController(RestController):
-    _root_path = "/channel_api/"
+    _root_path = "/channel-api/"
     _collection_name = "sale.import.rest.services"
     _default_auth = "api_key"
