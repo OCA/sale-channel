f0bdef605c6100b5cd5ace9b7dace92fc8ff8e95
Kevin Khao <kevinkhao@gmail.com>
2020-11-25 18:14:00 +0100

    [14.0][MIG] Migration to 14.0

--- sale_channel_partner/__manifest__.py
+++ sale_channel_partner/__manifest__.py
@@ -3,9 +3,9 @@
 {
     "name": "Sale Channel Partner",
     "summary": "Bind sale channels to contacts",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules/Sale",
-    "author": "Akretion",
+    "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/akretion/sale-import",
     "depends": ["sale_channel"],
     "license": "AGPL-3",
@@ -16,5 +16,5 @@
         "views/sale_channel_partner.xml",
     ],
     "demo": ["demo/demo.xml"],
-    "installable": False,
+    "installable": True,
 }
