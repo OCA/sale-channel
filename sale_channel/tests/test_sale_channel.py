edece933a7488c3fc0c5694ee5ed16ced1c66771
Kevin Khao <kevinkhao@gmail.com>
2020-11-26 15:28:21 +0100

    sale_channel: Migration to 14.0

--- sale_channel/tests/test_sale_channel.py
+++ sale_channel/tests/test_sale_channel.py
@@ -1,10 +1,10 @@
 #  Copyright (c) Akretion 2020
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-from odoo.addons.sale.tests.test_sale_common import TestCommonSaleNoChart
+from odoo.tests.common import SavepointCase
 
 
-class TestSaleChannel(TestCommonSaleNoChart):
+class TestSaleChannel(SavepointCase):
     def setUp(self):
         super().setUp()
         self.sale_channel = self.env.ref("sale_channel.sale_channel_amazon")
@@ -16,7 +16,8 @@ class TestSaleChannel(TestCommonSaleNoChart):
         self.sale_order.order_line.mapped("product_id").write(
             {"invoice_policy": "order"}
         )
-        self.sale_order.action_invoice_create()
-        self.assertEqual(
-            self.sale_order.invoice_ids[0].sale_channel_id, self.sale_channel
-        )
+        # refresh quantities: onchange trigger is on SO state,
+        # not on invoice_policy
+        self.sale_order.order_line._get_to_invoice_qty()
+        self.sale_order._create_invoices()
+        self.assertEqual(self.sale_order.invoice_ids.sale_channel_id, self.sale_channel)
