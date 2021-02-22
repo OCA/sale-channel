1971facf5672aac99418d6049100822409debe23
Sébastien BEAU <sebastien.beau@akretion.com>
2021-02-22 01:10:04 +0100

    sale_channel_product: add module

---
+++ sale_channel_product/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Channel Product",
+    "summary": "Link Product with sale channel",
+    "version": "16.0.1.0.1",
+    "category": "Sale Channel",
+    "website": "https://github.com/OCA/sale-channel",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "sale_channel",
+    ],
+    "data": [
+        "views/product_template_view.xml",
+    ],
+    "demo": [],
+}
