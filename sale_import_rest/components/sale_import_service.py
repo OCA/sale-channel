5c7483c0914a5174a2793339a473ce296bae17d0
Sébastien BEAU <sebastien.beau@akretion.com>
2022-10-11 16:07:11 +0200

    Fix copyright header licence is AGPL3 in manifest

--- sale_import_rest/components/sale_import_service.py
+++ sale_import_rest/components/sale_import_service.py
@@ -1,5 +1,5 @@
 #  Copyright (c) Akretion 2020
-#  License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 import json
 
 from odoo import _
