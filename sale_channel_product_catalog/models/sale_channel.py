98d91d4fb0526f6543a51a03b123bed571960b41
hparfr <raphael.reverdy@akretion.com>
2023-06-06 16:20:06 +0200

    sale_channel_product_catalog: add module
    Link a catalog to a sale_channel

---
+++ sale_channel_product_catalog/models/sale_channel.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Raphaël Reverdy<raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleChannel(models.Model):
+    _inherit = "sale.channel"
+
+    product_catalog = fields.Many2one("product.catalog")
