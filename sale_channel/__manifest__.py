627a58970206cad499bbba99d0c13cb34ea3f51b
Sébastien BEAU <sebastien.beau@akretion.com>
2023-06-07 13:47:22 +0200

    sale-channel: bump

--- sale_channel/__manifest__.py
+++ sale_channel/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Channel",
     "summary": "Adds the notion of sale channels",
-    "version": "16.0.0.0.1",
+    "version": "16.0.0.0.2",
     "category": "Generic Modules/Sale",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-channel",
