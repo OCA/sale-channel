94791ec6c10c821686a1fa55d9bd89b189be3c7e
hparfr <raphael.reverdy@akretion.com>
2023-06-21 14:32:43 +0200

    fixup! sale_channel_search_engine_product_catalog: add module

---
+++ sale_channel_search_engine_product_catalog/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Raphaël Reverdy<raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Channel Search Engine Product Catalog",
+    "summary": "export product through catalog to se",
+    "version": "16.0.1.0.0",
+    "category": "Sale Channel",
+    "website": "https://github.com/OCA/sale-channel",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_channel_product_catalog",
+        "sale_channel_search_engine",
+    ],
+}
