57b20b35aff8dba14c91863ed74da5b5f2147e4e
hparfr <raphael.reverdy@akretion.com>
2023-08-22 13:02:48 +0200

    sale_channel_search_engine: add smart button to bindings

--- sale_channel_search_engine/models/sale_channel.py
+++ sale_channel_search_engine/models/sale_channel.py
@@ -13,3 +13,10 @@ class SaleChannel(models.Model):
     ]
 
     search_engine_id = fields.Many2one("se.backend", "Search Engine")
+
+    def open_se_binding(self):
+        action = self.env.ref("connector_search_engine.se_binding_action").read()[0]
+        indexes = self.search_engine_id.index_ids.ids
+
+        action["domain"] = [("index_id", "in", indexes)]
+        return action
