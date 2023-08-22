2a2c669a53502d412e6bd0717a529f0499f3b9e2
hparfr <raphael.reverdy@akretion.com>
2023-08-22 07:24:45 +0200

    sale_channel_seach_engine: contextualize with sale_channel
    inject sale_channel into the context during the recompute of the binding
    having the sale_channel is important for example to filter out the categories
    not published

--- sale_channel_search_engine/models/sale_channel.py
+++ sale_channel_search_engine/models/sale_channel.py
@@ -8,4 +8,8 @@ from odoo import fields, models
 class SaleChannel(models.Model):
     _inherit = "sale.channel"
 
+    _sql_constraints = [
+        ("se_uniq", "unique (search_engine_id)", "Only one backend per search engine")
+    ]
+
     search_engine_id = fields.Many2one("se.backend", "Search Engine")
