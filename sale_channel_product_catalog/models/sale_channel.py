71a17292f42bcb06ab7b81ef4c3cd734ebc66751
hparfr <raphael.reverdy@akretion.com>
2023-09-08 23:22:43 +0200

    fixup! sale_channel_product_catalog: add module

--- sale_channel_product_catalog/models/sale_channel.py
+++ sale_channel_product_catalog/models/sale_channel.py
@@ -9,3 +9,19 @@ class SaleChannel(models.Model):
     _inherit = "sale.channel"
 
     product_catalog = fields.Many2one("product.catalog")
+
+    def is_add_to_sol_authorized(self, product, **kwargs):
+        # product_id should exist
+        # return true or false if this product can add the product in so
+        # inactive products are always exluded
+        # not sellable products are always excluded
+        # if you need to sell inactive product, make unarchive them before
+
+        # this method is a basic check to limit data leak
+        # if someone try to add an unauthorized product
+
+        # is product in the catalog ?
+        if self.product_catalog.id in product.product_catalog_ids.ids:
+            return product.active and product.sale_ok
+        else:
+            return False
