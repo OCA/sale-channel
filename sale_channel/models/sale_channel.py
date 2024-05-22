53ba21a4ac45eeeb125d62fd035d96b282edd83e
clementmbr <clement.mombereau@akretion.com.br>
2024-05-22 18:27:38 -0300

    [IMP] add channel_type fields

--- sale_channel/models/sale_channel.py
+++ sale_channel/models/sale_channel.py
@@ -10,3 +10,8 @@ class SaleChannel(models.Model):
 
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
+    company_id = fields.Many2one("res.company", default=lambda self: self.env.company)
+    channel_type = fields.Selection(
+        [],
+        help="Allows to use specific fields and actions for a specific channel's type",
+    )
