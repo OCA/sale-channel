e29bca3baa00b5f4df98dc28425940f8611e7169
Mathieu <mathieu.delva@hotmail.com>
2024-07-08 10:25:58 +0200

    [ADD] sale_channel_notification

---
+++ sale_channel_notification/models/sale_channel.py
@@ -0,0 +1,35 @@
+# Copyright 2024 Akretion (https://www.akretion.com).
+# @author Mathieu Delva <mathieu.delva@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+from odoo.tools.translate import _
+
+
+class SaleChannel(models.Model):
+    _inherit = "sale.channel"
+
+    notification_ids = fields.One2many(
+        "sale.channel.notification",
+        "sale_channel_id",
+        "Notification",
+        help="Send mail for predefined events",
+    )
+
+    def _send_notification(self, notification, record):
+        self.ensure_one()
+        record.ensure_one()
+        notifs = self.env["sale.channel.notification"].search(
+            [
+                ("sale_channel_id", "=", self.id),
+                ("notification_type", "=", notification),
+            ]
+        )
+        description = _("Notify %s for %s,%s") % (
+            notification,
+            record._name,
+            record.id,
+        )
+        for notif in notifs:
+            notif.send(record.id)
+        return True
