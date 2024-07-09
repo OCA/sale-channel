8b2e7e10e394ae3682cf6ed91ee4561cc8907409
Mathieu <mathieu.delva@hotmail.com>
2024-07-09 10:29:03 +0200

    [FIX] pylint

--- sale_channel_notification/models/stock_picking.py
+++ sale_channel_notification/models/stock_picking.py
@@ -2,8 +2,7 @@
 # @author Mathieu Delva <mathieu.delva@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
-from odoo.tools.translate import _
+from odoo import api, models
 
 
 class StockPicking(models.Model):
