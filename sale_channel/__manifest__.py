fe390c11beeb237d6fbe97ed6bd64c5921479c47
François Honoré <francois.honore@acsone.eu>
2026-06-01 13:25:03 +0200

    [MIG] sale_channel: Migration to 19.0
    - Bump version to 19.0.1.0.0
    - Rework tests to not rely on demo data (no longer installed by default
      in 19.0): build the data with AccountTestInvoicingCommon and grant the
      sales manager group via the new group_ids field.

--- sale_channel/__manifest__.py
+++ sale_channel/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Channel",
     "summary": "Adds the notion of sale channels",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "category": "Generic Modules/Sale",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-channel",
--- sale_channel/tests/test_sale_channel.py
+++ sale_channel/tests/test_sale_channel.py
@@ -1,23 +1,37 @@
 #  Copyright (c) Akretion 2020
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-from odoo.tests.common import TransactionCase
+from odoo import Command
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-class TestSaleChannel(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.sale_channel = self.env.ref("sale_channel.sale_channel_amazon")
-        self.sale_order = self.env.ref("sale.sale_order_4")
-        self.partner = self.env.ref("base.res_partner_4")
+class TestSaleChannel(AccountTestInvoicingCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # AccountTestInvoicingCommon runs as a restricted accounting user; grant
+        # sales rights so it can manage sale channels and confirm sale orders.
+        cls.env.user.group_ids |= cls.env.ref("sales_team.group_sale_manager")
+        cls.sale_channel = cls.env["sale.channel"].create({"name": "Amazon"})
+        cls.product = cls.product_a
+        cls.product.invoice_policy = "order"
+        cls.sale_order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner_a.id,
+                "order_line": [
+                    Command.create(
+                        {
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 1,
+                        }
+                    )
+                ],
+            }
+        )
 
     def test_sale_channel(self):
         self.sale_order.sale_channel_id = self.sale_channel
-        self.sale_order.order_line.mapped("product_id").write(
-            {"invoice_policy": "order"}
-        )
-        # Force to recompute qty_to_invoice as we have change the product
-        # invoice policy
-        self.sale_order.order_line._compute_qty_to_invoice()
+        self.sale_order.action_confirm()
         self.sale_order._create_invoices()
         self.assertEqual(self.sale_order.invoice_ids.sale_channel_id, self.sale_channel)
